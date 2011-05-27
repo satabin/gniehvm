1960d0f75133f063b772359456d453a37edff368
Lucas Satabin <lucassat@n7mm.org>
2011-05-27 08:42:14 +0200

    mark constants as const

--- class-parser.js
+++ class-parser.js
@@ -27,43 +27,43 @@ Parser = function(buffer/*: ArrayBuffer */){
 };
 
 // constant types
-CONSTANT_Class = 7;
-CONSTANT_Fieldref = 9;
-CONSTANT_Methodref = 10;
-CONSTANT_InterfaceMethodref = 11;
-CONSTANT_String = 8;
-CONSTANT_Integer = 3;
-CONSTANT_Float = 4;
-CONSTANT_Long = 5;
-CONSTANT_Double = 6;
-CONSTANT_NameAndType = 12;
-CONSTANT_Utf8 = 1;
+const CONSTANT_Class = 7;
+const CONSTANT_Fieldref = 9;
+const CONSTANT_Methodref = 10;
+const CONSTANT_InterfaceMethodref = 11;
+const CONSTANT_String = 8;
+const CONSTANT_Integer = 3;
+const CONSTANT_Float = 4;
+const CONSTANT_Long = 5;
+const CONSTANT_Double = 6;
+const CONSTANT_NameAndType = 12;
+const CONSTANT_Utf8 = 1;
 
 // modifiers
-ACC_PUBLIC = 0x0001;
-ACC_PRIVATE = 0x0002
-ACC_PROTECTED = 0x0004;
-ACC_STATIC = 0x0008;
-ACC_FINAL = 0x0010;
-ACC_SUPER = 0x0020;
-ACC_SYNCHRONIZED = 0x0020;
-ACC_VOLATILE = 0x0040;
-ACC_TRANSIENT = 0x0080;
-ACC_NATIVE = 0x0100;
-ACC_INTERFACE = 0x0200;
-ACC_ABSTRACT = 0x0400;
-ACC_STRICT = 0x0800;
+const ACC_PUBLIC = 0x0001;
+const ACC_PRIVATE = 0x0002
+const ACC_PROTECTED = 0x0004;
+const ACC_STATIC = 0x0008;
+const ACC_FINAL = 0x0010;
+const ACC_SUPER = 0x0020;
+const ACC_SYNCHRONIZED = 0x0020;
+const ACC_VOLATILE = 0x0040;
+const ACC_TRANSIENT = 0x0080;
+const ACC_NATIVE = 0x0100;
+const ACC_INTERFACE = 0x0200;
+const ACC_ABSTRACT = 0x0400;
+const ACC_STRICT = 0x0800;
 
 // attributes
-ATTR_ConstantValue = "ConstantValue";
-ATTR_Code = "Code";
-ATTR_Exceptions = "Exceptions";
-ATTR_InnerClasses = "InnerClasses";
-ATTR_Synthetic = "Synthetic";
-ATTR_SourceFile = "SourceFile";
-ATTR_LineNumberTable = "LineNumberTable";
-ATTR_LocalVariableTable = "LocalVariableTable";
-ATTR_Deprecated = "Deprecated";
+const ATTR_ConstantValue = "ConstantValue";
+const ATTR_Code = "Code";
+const ATTR_Exceptions = "Exceptions";
+const ATTR_InnerClasses = "InnerClasses";
+const ATTR_Synthetic = "Synthetic";
+const ATTR_SourceFile = "SourceFile";
+const ATTR_LineNumberTable = "LineNumberTable";
+const ATTR_LocalVariableTable = "LocalVariableTable";
+const ATTR_Deprecated = "Deprecated";
 
 /* parses the stream and returns the corresponding class object */
 Parser.prototype.parse = function() {
