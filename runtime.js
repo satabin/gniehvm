28707df27e835e8d2c262b5b0ad749ce1ccf5305
Lucas Satabin <lucassat@n7mm.org>
2011-05-30 23:49:40 +0200

    syntax

--- runtime.js
+++ runtime.js
@@ -6,15 +6,15 @@
 Frame = function() {
   // the program counter for this frame
   this.pc = 0;
-}
+};
 
 Interpreter = function() {
-}
+};
 
-Interpreter.prototype.execute(clazz/*: Class*/) {
+Interpreter.prototype.execute = function(clazz/*: Class*/) {
   var instructions = clazz.instructions;
   for(i in instructions) {
     // fetch the instruction
     var instruction = instructions[i];
   }
-}
+};
