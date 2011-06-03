6ce1497dfeb8e15fd9ac6a2683c9a2433e9a2a7f
Lucas Satabin <lucassat@n7mm.org>
2011-06-03 18:31:26 +0200

    exceptions management in the vm added

---
+++ exceptions.js
@@ -0,0 +1,8 @@
+throwException = function(frame/*: Frame*/, class_name/*: String*/) {
+  // TODO construct the call stack for the current frame and throws the exception
+  // of the given name
+}
+
+NoClassDefFoundError = function(name) {
+  this.name = name;
+}
