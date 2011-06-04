311c8698eaf0a286d5dad8312a2f550e1e12dd1f
Lucas Satabin <lucassat@n7mm.org>
2011-06-04 19:11:25 +0200

    new tests

--- test/Test.java
+++ test/Test.java
@@ -1,6 +1,7 @@
 interface Itf {}
 public class Test implements Itf {
   private long f = 0L;
+  private String s = "éüße";
   public int m(String s, int i) {
     System.out.println(s);
     int j = i * 3;
